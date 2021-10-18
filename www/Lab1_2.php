8b8290c5ec2b97b068790a0a837778a2cb804a59
Rondol <wedolak@yandex.ru>
2021-10-18 22:00:08 +0500

    Теперь и красным

--- www/Lab1_2.php
+++ www/Lab1_2.php
@@ -1,15 +1,22 @@
 <HTML>
 <TITLE> Таблица умножения </TITLE>
 <BODY>
+
+<p> Супер табличка: <br>
+<p>
+
 <TABLE border=1>
 <?php
-print ("<p> Супер табличка: <br>");
 for ($i=0; $i<=9; $i++) { // запускаем первый цикл
     echo ("<tr>"); // начало строки таблицы
     for ($j=1; $j<=10; $j++) { // запускаем второй цикл
         echo ("<td align=center>"); // открываем ячейку таблицы
-        $inTableNum = $j+$i*10; // находим произведение двух чисел и...
-        echo ($inTableNum); // выводим его,
+        $inTableNum = $j+$i*10; // находим число
+        if ($inTableNum%2==0) {
+            echo ('<p><font color="red">'.$inTableNum.'</font>'); // выводим его,
+        } else {
+            echo ($inTableNum);
+        }
         echo ("</td>"); // а потом закрываем ячейку
     }
     echo ("</tr>"); // конец строки таблицы
