0dca6340e8e2261cc0d5cec2dcc3bed0ad385cfd
Rondol <wedolak@yandex.ru>
2021-10-18 21:41:27 +0500

    Начало работы над второй работой

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <ul>
-	<li><a href="www/Lab1_2.php">Первая самостоятельная</a></li>
+	<li><a href="www/Lab1_1.php">Первая самостоятельная</a></li>
+	<li><a href="www/Lab1_2.php">Вторая самостоятельная</a></li>
 </ul>
