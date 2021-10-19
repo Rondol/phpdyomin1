cba76a74ad8b23a30095578e5200306330f7b4f7
Rondol <wedolak@yandex.ru>
2021-10-19 15:10:49 +0500

    Начало работы над работой 1_3

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <ul>
-	<li><a href="www/Lab1_1.php">Первая самостоятельная</a></li>
-	<li><a href="www/Lab1_2.php">Вторая самостоятельная</a></li>
+	<li>Первая лабораторная</li>
+	<ul>
+	<li><a href="www/Lab1_1.php">Первая работа</a></li>
+	<li><a href="www/Lab1_2.php">Вторая работа</a></li>
+	<li><a href="www/Lab1_2.php">Третья работа</a></li>
+	</ul>
 </ul>
