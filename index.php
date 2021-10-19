292c7b666f87f4a6ec4ce0b0057515346c10ad6d
Rondol <wedolak@yandex.ru>
2021-10-19 16:05:17 +0500

    Исправление ссылок в index

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <ul>
 	<li>Первая лабораторная</li>
 	<ul>
-	<li><a href="www/Lab1_1.php">Первая работа</a></li>
-	<li><a href="www/Lab1_2.php">Вторая работа</a></li>
-	<li><a href="www/Lab1_3.php">Третья работа</a></li>
+	<li><a href="Lab1_1.php">Первая работа</a></li>
+	<li><a href="Lab1_2.php">Вторая работа</a></li>
+	<li><a href="Lab1_3.php">Третья работа</a></li>
 	</ul>
 </ul>
 </body>
