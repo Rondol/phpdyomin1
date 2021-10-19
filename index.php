b33daf74dc5d78b105a4677315a17721f209a8e1
Rondol <wedolak@yandex.ru>
2021-10-19 15:51:55 +0500

    Сделана работа 1_3

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<html>
+<body>
 <ul>
 	<li>Первая лабораторная</li>
 	<ul>
 	<li><a href="www/Lab1_1.php">Первая работа</a></li>
 	<li><a href="www/Lab1_2.php">Вторая работа</a></li>
-	<li><a href="www/Lab1_2.php">Третья работа</a></li>
+	<li><a href="www/Lab1_3.php">Третья работа</a></li>
 	</ul>
 </ul>
+</body>
+</html>
