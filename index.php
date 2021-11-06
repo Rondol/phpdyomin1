60d4396eac61037de938372ded630662353b0f47
Rondol <wedolak@yandex.ru>
2021-11-06 14:43:29 +0500

    Доделаны вторые варианты первой лабораторной работы

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	<ul>
 	<li><a href="Lab1_1.php">Первая работа</a></li>
 	<li><a href="Lab1_2.php">Вторая работа</a></li>
-	<li><a href="Lab1_3.php">Третья работа</a></li>
+	<li><a href="Lab1_3.php">Третья работа(1 вариант)</a></li>
+	<li><a href="Lab1_4.php">Третья работа(2 вариант)</a></li>
 	</ul>
 </ul>
 </body>
