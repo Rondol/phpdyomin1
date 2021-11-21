aecf0be874086856e72360ce00614dbda1c948e0
Rondol <wedolak@yandex.ru>
2021-11-21 12:26:30 +0500

    Начало работы над третьей лабораторной

--- index.php
+++ index.php
@@ -20,6 +20,15 @@
 	<li><a href="web/Lab2/Lab2_5.php">Пятая работа</a></li>
 	<li><a href="web/Lab2/Lab2_6.php">Шестая работа</a></li>
 	</ul>
+	<li>Вторая лабораторная</li>
+	<ul>
+	<li><a href="web/Lab3/Lab3_1.php">Первая работа</a></li>
+	<li><a href="web/Lab3/Lab3_2.php">Вторая работа</a></li>
+	<li><a href="web/Lab3/Lab3_3.php">Третья работа</a></li>
+	<li><a href="web/Lab3/Lab3_4.php">Четвёртая работа</a></li>
+	<li><a href="web/Lab3/Lab3_5.php">Пятая работа</a></li>
+	<li><a href="web/Lab3/Lab3_6.php">Шестая работа</a></li>
+	</ul>
 </ul>
 </body>
 </html>
